0a6e13c93ba1864715d383f63683ac3c1c4396b1
Rajat Pundir <rajatpundir13@gmail.com>
2022-01-17 20:47:41 +0530

    Autosave

--- src/views/Test.tsx
+++ src/views/Test.tsx
@@ -24,161 +24,154 @@ const views = {
   User: UserViews,
 };
 
+const common_default_component: ComponentViews[string]["create"] = (props) => {
+  const navigation = useNavigation();
+  return (
+    <ScrollView m={"2"}>
+      <Template
+        {...props}
+        type={"CLA"}
+        fields={["str", [["z"], "str"], "lstr", "clob"]}
+      />
+      <Template
+        {...props}
+        type={"CLB"}
+        fields={[
+          "i32",
+          {
+            path: "u32",
+            checks: [{ name: "u32_is_even", message: "U32 cannot be odd" }],
+          },
+          "i64",
+          "u64",
+          "idouble",
+          "udouble",
+          "idecimal",
+          "udecimal",
+        ]}
+      />
+      <Template {...props} type={"RLA"} fields={["bool", "date"]} />
+      <Template {...props} type={"RLA"} fields={["time", "timestamp"]} />
+      <Template
+        {...props}
+        type={"CLA"}
+        fields={[
+          {
+            path: "user",
+            options: [
+              "other",
+              {
+                title: "Select User",
+                limit: new Decimal(10),
+                user_paths: [],
+                borrows: [],
+                labels: [
+                  ["Nickname", [[], "nickname"]],
+                  ["Knows english", [[], "knows_english"]],
+                  ["Mobile", [[], "mobile"]],
+                  ["Product Count", [[], "product_count"]],
+                ],
+                element: arrow(() => {
+                  const result = get_path(props.state, [[], "user"]);
+                  if (unwrap(result)) {
+                    const path = result.value;
+                    if (path.writeable) {
+                      const value = path.path[1][1];
+                      if (value.type === "other" && value.value.equals(-1)) {
+                        return (
+                          <Row>
+                            <Text>Select User </Text>
+                            {props.state.mode === "write" ? (
+                              <FontAwesome
+                                name="edit"
+                                size={24}
+                                color={theme.placeholder}
+                              />
+                            ) : (
+                              <></>
+                            )}
+                          </Row>
+                        );
+                      } else {
+                        return (
+                          <Row>
+                            <Field {...props} path={[["user"], "nickname"]} />
+                            <Text> </Text>
+                            {props.state.mode === "write" ? (
+                              <FontAwesome
+                                name="edit"
+                                size={24}
+                                color={theme.placeholder}
+                              />
+                            ) : (
+                              <></>
+                            )}
+                          </Row>
+                        );
+                      }
+                    }
+                  }
+                  return <></>;
+                }),
+                render_list_element: [
+                  (props) => (
+                    <OtherComponent {...props} view={views.User["Default"]} />
+                  ),
+                  {},
+                ],
+                render_custom_fields: (props) => (
+                  <SearchBar
+                    {...props}
+                    placeholder="Nickname"
+                    path={[[], "nickname"]}
+                  />
+                ),
+              },
+            ],
+          },
+        ]}
+      />
+      <Row justifyContent={"flex-end"} my={"2"}>
+        <Pressable
+          onPress={() => navigation.goBack()}
+          style={tw.style(["px-4", "py-2", "rounded", "border", "mx-2"], {
+            borderColor: theme.primary,
+          })}
+        >
+          <Text>Cancel</Text>
+        </Pressable>
+        <Pressable
+          onPress={async () => {
+            try {
+              await replace_variable(
+                new Decimal(0),
+                new Variable(
+                  props.struct,
+                  props.state.id as Decimal,
+                  props.state.active,
+                  props.state.created_at,
+                  props.state.updated_at,
+                  props.state.values as HashSet<Path>
+                )
+              );
+              navigation.goBack();
+            } catch (e) {}
+          }}
+          style={tw.style(["px-4", "py-2", "rounded"], {
+            backgroundColor: theme.primary,
+          })}
+        >
+          <Text fontWeight={"bold"}>Save</Text>
+        </Pressable>
+      </Row>
+    </ScrollView>
+  );
+};
+
 export default {
   Default: {
-    create: (props) => {
-      const navigation = useNavigation();
-      return (
-        <ScrollView m={"2"}>
-          <Template
-            type={"CLA"}
-            {...props}
-            fields={["str", [["z"], "str"], "lstr", "clob"]}
-          />
-          <Template
-            type={"CLB"}
-            {...props}
-            fields={[
-              "i32",
-              {
-                path: "u32",
-                checks: [{ name: "u32_is_even", message: "U32 cannot be odd" }],
-              },
-              "i64",
-              "u64",
-              "idouble",
-              "udouble",
-              "idecimal",
-              "udecimal",
-            ]}
-          />
-          <Template type={"RLA"} {...props} fields={["bool", "date"]} />
-          <Template type={"RLA"} {...props} fields={["time", "timestamp"]} />
-          <Template
-            type={"CLA"}
-            {...props}
-            fields={[
-              {
-                path: "user",
-                options: [
-                  "other",
-                  {
-                    title: "Select User",
-                    limit: new Decimal(10),
-                    user_paths: [],
-                    borrows: [],
-                    labels: [
-                      ["Nickname", [[], "nickname"]],
-                      ["Knows english", [[], "knows_english"]],
-                      ["Mobile", [[], "mobile"]],
-                      ["Product Count", [[], "product_count"]],
-                    ],
-                    element: arrow(() => {
-                      const result = get_path(props.state, [[], "user"]);
-                      if (unwrap(result)) {
-                        const path = result.value;
-                        if (path.writeable) {
-                          const value = path.path[1][1];
-                          if (
-                            value.type === "other" &&
-                            value.value.equals(-1)
-                          ) {
-                            return (
-                              <Row>
-                                <Text>Select User </Text>
-                                {props.state.mode === "write" ? (
-                                  <FontAwesome
-                                    name="edit"
-                                    size={24}
-                                    color={theme.placeholder}
-                                  />
-                                ) : (
-                                  <></>
-                                )}
-                              </Row>
-                            );
-                          } else {
-                            return (
-                              <Row>
-                                <Field
-                                  {...props}
-                                  path={[["user"], "nickname"]}
-                                />
-                                <Text> </Text>
-                                {props.state.mode === "write" ? (
-                                  <FontAwesome
-                                    name="edit"
-                                    size={24}
-                                    color={theme.placeholder}
-                                  />
-                                ) : (
-                                  <></>
-                                )}
-                              </Row>
-                            );
-                          }
-                        }
-                      }
-                      return <></>;
-                    }),
-                    render_list_element: [
-                      (props) => (
-                        <OtherComponent
-                          {...props}
-                          view={views.User["Default"]}
-                        />
-                      ),
-                      {},
-                    ],
-                    render_custom_fields: (props) => (
-                      <SearchBar
-                        {...props}
-                        placeholder="Nickname"
-                        path={[[], "nickname"]}
-                      />
-                    ),
-                  },
-                ],
-              },
-            ]}
-          />
-          <Row justifyContent={"flex-end"} my={"2"}>
-            <Pressable
-              onPress={() => navigation.goBack()}
-              style={tw.style(["px-4", "py-2", "rounded", "border", "mx-2"], {
-                borderColor: theme.primary,
-              })}
-            >
-              <Text>Cancel</Text>
-            </Pressable>
-            <Pressable
-              onPress={async () => {
-                try {
-                  await replace_variable(
-                    new Decimal(0),
-                    new Variable(
-                      props.struct,
-                      props.state.id as Decimal,
-                      props.state.active,
-                      props.state.created_at,
-                      props.state.updated_at,
-                      props.state.values as HashSet<Path>
-                    )
-                  );
-                  navigation.goBack();
-                } catch (e) {}
-              }}
-              style={tw.style(["px-4", "py-2", "rounded"], {
-                backgroundColor: theme.primary,
-              })}
-            >
-              <Text fontWeight={"bold"}>Save</Text>
-            </Pressable>
-          </Row>
-        </ScrollView>
-      );
-    },
-    update: () => <></>,
-    show: () => <></>,
+    create: common_default_component,
+    update: common_default_component,
+    show: common_default_component,
   },
 } as ComponentViews;
--- src/views/User.tsx
+++ src/views/User.tsx
@@ -7,56 +7,58 @@ import { arrow } from "../lib/utils/prelude";
 import { colors, tw } from "../lib/utils/tailwind";
 import { theme } from "../lib/utils/theme";
 
+const common_default_component: ComponentViews[string]["show"] = (props) => {
+  return (
+    <Column
+      p={"2"}
+      m={"1"}
+      borderWidth={"1"}
+      borderRadius={"md"}
+      borderColor={colors.slate[600]}
+      backgroundColor={arrow(() => {
+        if (props.selected) {
+          return colors.slate[800];
+        }
+        return colors.slate[900];
+      })}
+    >
+      <Row justifyContent={"space-between"}>
+        <Column>
+          <Text>Unique ID</Text>
+        </Column>
+        <Column>
+          <Text>{props.state.id.toString()}</Text>
+        </Column>
+      </Row>
+      <Template
+        {...props}
+        type={"CLB"}
+        fields={["nickname", "knows_english", "mobile", "product_count"]}
+      />
+      <Row justifyContent={"flex-end"}>
+        {arrow(() => {
+          if (!props.selected) {
+            return (
+              <Pressable
+                onPress={props.update_parent_values}
+                style={tw.style(["px-2", "py-1", "rounded", "border"], {
+                  borderColor: theme.placeholder,
+                })}
+              >
+                <Text fontWeight={"bold"}>OK</Text>
+              </Pressable>
+            );
+          }
+        })}
+      </Row>
+    </Column>
+  );
+};
+
 export default {
   Default: {
     create: () => <></>,
-    update: () => <></>,
-    show: (props) => {
-      return (
-        <Column
-          p={"2"}
-          m={"1"}
-          borderWidth={"1"}
-          borderRadius={"md"}
-          borderColor={colors.slate[600]}
-          backgroundColor={arrow(() => {
-            if (props.selected) {
-              return colors.slate[800];
-            }
-            return colors.slate[900];
-          })}
-        >
-          <Row justifyContent={"space-between"}>
-            <Column>
-              <Text>Unique ID</Text>
-            </Column>
-            <Column>
-              <Text>{props.state.id.toString()}</Text>
-            </Column>
-          </Row>
-          <Template
-            type={"CLB"}
-            {...props}
-            fields={["nickname", "knows_english", "mobile", "product_count"]}
-          />
-          <Row justifyContent={"flex-end"}>
-            {arrow(() => {
-              if (!props.selected) {
-                return (
-                  <Pressable
-                    onPress={props.update_parent_values}
-                    style={tw.style(["px-2", "py-1", "rounded", "border"], {
-                      borderColor: theme.placeholder,
-                    })}
-                  >
-                    <Text fontWeight={"bold"}>OK</Text>
-                  </Pressable>
-                );
-              }
-            })}
-          </Row>
-        </Column>
-      );
-    },
+    update: common_default_component,
+    show: common_default_component,
   },
 } as ComponentViews;
