a4bedda838048e24133d52d47c52b26cd63322ce
Rajat Pundir <rajatpundir13@gmail.com>
2022-02-11 17:01:01 +0530

    Autosave

--- src/navigation/loader/linker/url/index.tsx
+++ src/navigation/loader/linker/url/index.tsx
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
 import { NavigatorProps as ParentNavigatorProps } from "..";
-import { Column, Input, Pressable, Row, Text } from "native-base";
+import { Column, Input, Pressable, Row, Text, Image } from "native-base";
 import { MaterialIcons } from "@expo/vector-icons";
 import { useTheme } from "../../../../lib/utils/theme";
+import { apply, arrow } from "../../../../lib/utils/prelude";
 
 export default function Component(props: ParentNavigatorProps<"URL">) {
   const theme = useTheme();
   const [local_val, set_local_val] = useState("");
   const [has_errors, set_has_errors] = useState(false);
   const default_value = "";
+  const [url, set_url] = useState(undefined as URL | undefined);
+  const check_url = async (url: string) => (await fetch(url)).ok;
   return (
     <>
       <Column p={"2"}>
@@ -25,21 +28,18 @@ export default function Component(props: ParentNavigatorProps<"URL">) {
             placeholder={"Paste link to your resource (image, pdf, etc)"}
             value={local_val}
             isInvalid={has_errors}
-            onChangeText={(x) => {
+            onChangeText={async (x) => {
               try {
+                set_url(undefined);
                 set_local_val(x);
                 set_has_errors(false);
-                // props.dispatch([
-                //   "value",
-                //   apply(props.path, (it) => {
-                //     it.path[1][1] = {
-                //       type: value.type,
-                //       value: x,
-                //     };
-                //     return it;
-                //   }),
-                // ]);
+                const val = new URL(x);
+                const result = await check_url(val.toString());
+                if (result) {
+                  set_url(val);
+                }
               } catch (e) {
+                console.log(e);
                 set_has_errors(true);
               }
             }}
@@ -50,16 +50,7 @@ export default function Component(props: ParentNavigatorProps<"URL">) {
                   onPress={() => {
                     set_local_val(default_value);
                     set_has_errors(false);
-                    // props.dispatch([
-                    //   "value",
-                    //   apply(props.path, (it) => {
-                    //     it.path[1][1] = {
-                    //       type: value.type,
-                    //       value: default_value,
-                    //     };
-                    //     return it;
-                    //   }),
-                    // ]);
+                    set_url(undefined);
                   }}
                 >
                   <MaterialIcons
@@ -75,22 +66,38 @@ export default function Component(props: ParentNavigatorProps<"URL">) {
             borderColor={theme.border}
           />
         </Row>
-        <Row py={"2"} justifyContent={"flex-end"}>
-          <Pressable
-            justifyContent={"center"}
-            alignItems={"center"}
-            onPress={() => {}}
-            borderColor={theme.primary}
-            borderWidth={"1"}
-            borderRadius={"xs"}
-            px={"2"}
-            py={"0.5"}
-          >
-            <Text bold fontSize={"md"}>
-              Fetch
-            </Text>
-          </Pressable>
-        </Row>
+        {url !== undefined ? (
+          <Column my={"4"}>
+            <Row>
+              <Image
+                source={{
+                  uri: apply(url.toString(), (it) => it.replace(/\/+$/, "")),
+                }}
+                resizeMode="contain"
+                alt="Unable to load image, please check url"
+                width={"full"}
+                height={"48"}
+              />
+            </Row>
+            <Row justifyContent={"flex-end"}>
+              <Pressable
+                onPress={() => {}}
+                justifyContent={"center"}
+                alignItems={"center"}
+                backgroundColor={theme.primary}
+                borderRadius={"xs"}
+                px={"3"}
+                py={"1"}
+              >
+                <Text bold fontSize={"md"}>
+                  Add
+                </Text>
+              </Pressable>
+            </Row>
+          </Column>
+        ) : (
+          <></>
+        )}
       </Column>
     </>
   );
