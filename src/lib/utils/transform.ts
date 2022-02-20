5e1c4befc468c73b9523eeefac3dd159a7f9166f
Rajat Pundir <rajatpundir13@gmail.com>
2022-02-21 00:11:15 +0530

    Autosave

--- src/lib/utils/transform.ts
+++ src/lib/utils/transform.ts
@@ -1,8 +1,9 @@
 import Decimal from "decimal.js";
-import { ErrMsg } from "./errors";
+import { ErrMsg, errors } from "./errors";
 import { BooleanLispExpression, LispExpression } from "./lisp";
-import { Ok, Result } from "./prelude";
-import { PathString, StrongEnum } from "./variable";
+import { apply, arrow, CustomError, Err, Ok, Result, unwrap } from "./prelude";
+import { get_struct } from "./schema";
+import { compare_paths, PathString, StrongEnum } from "./variable";
 
 type TrandformQuery =
   | {
@@ -15,26 +16,32 @@ type TrandformQuery =
 
 type TransformInputs = Record<string, LispExpression>;
 
-type TransformArgs = ReadonlyArray<StrongEnum>;
+type TransformArgs = {
+  values: Record<string, StrongEnum>;
+  args: ReadonlyArray<Record<string, StrongEnum>>;
+};
 
 type TransformChecks = Record<string, [BooleanLispExpression, ErrMsg]>;
 
 export class Tranform {
   name: string;
-  fx: string;
+  type: "fx" | "compose";
+  invoke: string;
   query: TrandformQuery;
   inputs: TransformInputs;
   checks: TransformChecks;
 
   constructor(
     name: string,
-    fx: string,
+    type: "fx" | "compose",
+    invoke: string,
     query: TrandformQuery,
     inputs: TransformInputs,
     checks: TransformChecks
   ) {
     this.name = name;
-    this.fx = fx;
+    this.type = type;
+    this.invoke = invoke;
     this.query = query;
     this.inputs = inputs;
     this.checks = checks;
@@ -55,7 +62,81 @@ export class Tranform {
     return String(this.name);
   }
 
-  get_symbols(args: TransformArgs, level: Decimal) {}
+  get_symbols_for_query(
+    query: Exclude<TrandformQuery, undefined>,
+    values: TransformArgs["values"]
+  ) {
+    const struct = get_struct(query.struct);
+    if (unwrap(struct)) {
+      const paths: ReadonlyArray<PathString> = arrow(() => {
+        let paths: ReadonlyArray<PathString> = [];
+        for (const field_name of Object.keys(query.fields)) {
+          const expr = query.fields[field_name];
+          paths = paths.concat(expr.get_paths());
+        }
+        return apply([] as Array<PathString>, (it) => {
+          for (const path of paths) {
+            let check = true;
+            for (const existing_path of it) {
+              if (compare_paths(path, existing_path)) {
+                check = false;
+                break;
+              }
+            }
+            if (check) {
+              it.push(path);
+            }
+          }
+          return it;
+        });
+      });
+      console.log(paths);
+      // construct symbols based on paths
+      const symbols: Record<string, Symbol> = {};
+      for (const field_name of Object.keys(query.fields)) {
+        if (field_name in struct.value.fields) {
+          const field = struct.value.fields[field_name];
+          if (field_name in values) {
+            const value = values[field_name];
+            if (value.type === field.type) {
+            } else {
+              return new Err(new CustomError([errors.ErrUnexpected] as ErrMsg));
+            }
+          } else {
+            return new Err(new CustomError([errors.ErrUnexpected] as ErrMsg));
+          }
+        } else {
+          return new Err(new CustomError([errors.ErrUnexpected] as ErrMsg));
+        }
+      }
+    } else {
+      return new Err(new CustomError([errors.ErrUnexpected] as ErrMsg));
+    }
+  }
+
+  get_symbols(args: TransformArgs, level: Decimal) {
+    if (this.query !== undefined) {
+      const query = this.query;
+      const struct = get_struct(query.struct);
+      if (unwrap(struct)) {
+        for (const field_name in Object.keys(query.fields)) {
+          if (field_name in struct.value.fields) {
+            const field = struct.value.fields[field_name];
+            if (field_name in Object.keys(args.values)) {
+              const value = args.values[field_name];
+            } else {
+              return new Err(new CustomError([errors.ErrUnexpected] as ErrMsg));
+            }
+          } else {
+            return new Err(new CustomError([errors.ErrUnexpected] as ErrMsg));
+          }
+        }
+      } else {
+        return new Err(new CustomError([errors.ErrUnexpected] as ErrMsg));
+      }
+    } else {
+    }
+  }
 
   exec(
     args: TransformArgs,
