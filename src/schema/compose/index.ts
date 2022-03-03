7a7f648c30ef2d21ce52ae00ac1c03bda8257b28
Rajat Pundir <rajatpundir13@gmail.com>
2022-03-03 19:41:51 +0530

    Autosave

--- src/schema/compose/index.ts
+++ src/schema/compose/index.ts
@@ -66,8 +66,56 @@ const schema: Record<string, Compose> = {
   ),
   Create_Public_Resource: new Compose(
     "Create_Public_Resource",
-    {},
-    new ComposeStep(undefined, []),
+    {
+      resource_type: { type: "other", other: "Resource_Type" },
+      url: { type: "str" },
+      tags: { type: "list" },
+    },
+    new ComposeStep(undefined, [
+      {
+        name: "Create_Public_Resource",
+        type: "fx",
+        invoke: "Create_Public_Resource",
+        map: {
+          resource_type: { type: "input", value: "resource_type" },
+          url: { type: "input", value: "url" },
+        },
+        output: { public_resource: "public_resource" },
+      },
+      {
+        name: "Create_Tag",
+        type: "transform",
+        invoke: "Create_Tag",
+        map: {
+          base: {
+            type: "input",
+            value: "tags",
+          },
+          query: {},
+        },
+      },
+      {
+        type: "transform",
+        invoke: "Create_Public_Resource_Tag",
+        map: {
+          base: {
+            type: "transform",
+            value: "Create_Tag",
+            map: {
+              tag: "tag",
+            },
+            inject: {
+              public_resource: {
+                type: "fx",
+                value: ["Create_Public_Resource", "public_resource"],
+              },
+            },
+          },
+          query: {},
+        },
+        output: "tags",
+      },
+    ]),
     {}
   ),
 };
