f3e2894827f7b5b213797350fb6ca7223c7adbe7
Rajat Pundir <rajatpundir13@gmail.com>
2022-03-15 14:48:27 +0530

    Autosave

--- src/navigation/resources/community/index.tsx
+++ src/navigation/resources/community/index.tsx
@@ -1,110 +1,165 @@
 import * as React from "react";
+import Decimal from "decimal.js";
+import { HashSet } from "prelude-ts";
 
 import { NavigatorProps as ParentNavigatorProps } from "..";
-
 import {
-  createBottomTabNavigator,
-  BottomTabScreenProps,
-} from "@react-navigation/bottom-tabs";
-import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { CompositeScreenProps } from "@react-navigation/native";
+  OrFilter,
+  get_filter_paths,
+  OtherComponent,
+  Identity,
+  List,
+  Entrypoint,
+  useTheme,
+} from "../../../lib";
+import { get_struct } from "../../../schema";
+import { views } from "../../../views";
+import { Pressable, Row, Text } from "native-base";
+import { useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
 
-import { NavigatorParams as RootNavigatorParams } from "../../main";
-
-import Images from "./images";
-import Videos from "./videos";
-import Docs from "./docs";
-import YouTube from "./youtube";
-import { AntDesign, Entypo, Feather, Ionicons } from "@expo/vector-icons";
-import { tw } from "../../../lib";
-
-export type NavigatorParams = {
-  Images: undefined;
-  Videos: undefined;
-  Docs: undefined;
-  YouTube: undefined;
-};
-
-export default function Navigator(props: ParentNavigatorProps<"Community">) {
+export default function Component(props: ParentNavigatorProps<"Community">) {
+  const theme = useTheme();
+  const struct = get_struct("Private_Resource");
+  const entrypoints: Array<Entrypoint> = [[[], "owner"]];
+  const [resource_type, set_resource_type] = useState(
+    "image" as "image" | "video" | "pdf" | "youtube"
+  );
   return (
-    <BottomTab.Navigator
-      initialRouteName="Images"
-      screenOptions={{
-        lazy: true,
-        headerShown: false,
-        tabBarHideOnKeyboard: true,
-        tabBarLabelStyle: { fontSize: 12 },
-        tabBarItemStyle: { paddingTop: 2, paddingBottom: 4 },
-      }}
-    >
-      <BottomTab.Screen
-        name="Images"
-        component={Images}
-        options={{
-          title: "Images",
-          tabBarIcon: ({ color }) => (
-            <Ionicons
-              name="images"
-              size={24}
-              color={color}
-              style={tw.style([], { marginBottom: -6 })}
-            />
+    <>
+      <Row alignContent={"center"} mx={"1"} my={"2"}>
+        <Text fontSize={"md"} color={theme.label}>
+          Filter
+        </Text>
+        <Row alignContent={"center"} space={"2"} mx={"3"}>
+          <Pressable
+            flexDirection={"row"}
+            alignContent={"center"}
+            onPress={() => set_resource_type("image")}
+          >
+            {resource_type === "image" ? (
+              <Ionicons
+                name="radio-button-on"
+                size={18}
+                color={theme.primary}
+                style={{ marginTop: 1 }}
+              />
+            ) : (
+              <Ionicons
+                name="radio-button-off"
+                size={18}
+                color={theme.primary}
+                style={{ marginTop: 1 }}
+              />
+            )}
+            <Text>Images</Text>
+          </Pressable>
+          <Pressable
+            flexDirection={"row"}
+            onPress={() => set_resource_type("video")}
+          >
+            {resource_type === "video" ? (
+              <Ionicons
+                name="radio-button-on"
+                size={18}
+                color={theme.primary}
+                style={{ marginTop: 1 }}
+              />
+            ) : (
+              <Ionicons
+                name="radio-button-off"
+                size={18}
+                color={theme.primary}
+                style={{ marginTop: 1 }}
+              />
+            )}
+            <Text>Videos</Text>
+          </Pressable>
+          <Pressable
+            flexDirection={"row"}
+            onPress={() => set_resource_type("pdf")}
+          >
+            {resource_type === "pdf" ? (
+              <Ionicons
+                name="radio-button-on"
+                size={18}
+                color={theme.primary}
+                style={{ marginTop: 1 }}
+              />
+            ) : (
+              <Ionicons
+                name="radio-button-off"
+                size={18}
+                color={theme.primary}
+                style={{ marginTop: 1 }}
+              />
+            )}
+            <Text>Docs</Text>
+          </Pressable>
+          <Pressable
+            flexDirection={"row"}
+            onPress={() => set_resource_type("youtube")}
+          >
+            {resource_type === "youtube" ? (
+              <Ionicons
+                name="radio-button-on"
+                size={18}
+                color={theme.primary}
+                style={{ marginTop: 1 }}
+              />
+            ) : (
+              <Ionicons
+                name="radio-button-off"
+                size={18}
+                color={theme.primary}
+                style={{ marginTop: 1 }}
+              />
+            )}
+            <Text>YouTube</Text>
+          </Pressable>
+        </Row>
+      </Row>
+      <List
+        selected={new Decimal(-1)}
+        struct={struct}
+        filters={[
+          new OrFilter(
+            0,
+            [false, undefined],
+            [false, undefined],
+            [false, undefined],
+            get_filter_paths(
+              struct,
+              [
+                ["type", [["resource_type"], "type"]],
+                ["subtype", [["resource_type"], "subtype"]],
+                ["url", [[], "url"]],
+                ["tag_count", [[], "tag_count"]],
+                ["owner", [[], "owner"]],
+              ],
+              entrypoints
+            )
           ),
-        }}
+          HashSet.of(),
+        ]}
+        limit={new Decimal(10)}
+        options={[
+          "list",
+          {
+            entrypoints: entrypoints,
+            RenderElement: [
+              (props) => (
+                <OtherComponent
+                  {...props}
+                  view={views.Private_Resource["Default"]}
+                />
+              ),
+              {},
+            ],
+          },
+        ]}
+        RenderVariant={(props) => <Identity {...props} />}
       />
-      <BottomTab.Screen
-        name="Videos"
-        component={Videos}
-        options={{
-          title: "Videos",
-          tabBarIcon: ({ color }) => (
-            <Entypo
-              name="folder-video"
-              size={24}
-              color={color}
-              style={tw.style([], { marginBottom: -6 })}
-            />
-          ),
-        }}
-      />
-      <BottomTab.Screen
-        name="Docs"
-        component={Docs}
-        options={{
-          title: "Docs",
-          tabBarIcon: ({ color }) => (
-            <AntDesign
-              name="pdffile1"
-              size={24}
-              color={color}
-              style={tw.style([], { marginBottom: -4 })}
-            />
-          ),
-        }}
-      />
-      <BottomTab.Screen
-        name="YouTube"
-        component={YouTube}
-        options={{
-          title: "YouTube",
-          tabBarIcon: ({ color }) => (
-            <Feather
-              name="youtube"
-              size={24}
-              color={color}
-              style={tw.style([], { marginBottom: -6 })}
-            />
-          ),
-        }}
-      />
-    </BottomTab.Navigator>
+    </>
   );
 }
-
-export type NavigatorProps<Screen extends keyof NavigatorParams> =
-  CompositeScreenProps<
-    BottomTabScreenProps<NavigatorParams, Screen>,
-    NativeStackScreenProps<RootNavigatorParams>
-  >;
-
-const BottomTab = createBottomTabNavigator<NavigatorParams>();
--- src/navigation/resources/personal/docs/index.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-
-import { NavigatorProps as ParentNavigatorProps } from "..";
-
-export default function Component(props: ParentNavigatorProps<"Docs">) {
-  return <></>;
-}
